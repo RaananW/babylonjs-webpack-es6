77aa23da57f23a81ca5133bebc9a5b04e9a5f4ec
Raanan Weber <info@raananweber.com>
2020-04-22 15:14:12 +0200

    config and package files

---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// App directory
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = {
+    entry: path.resolve(appDirectory, "src"),
+    output: {
+        filename: 'js/babylonBundle.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|mjs|jsx|ts|tsx)$/,
+                loader: 'source-map-loader',
+                enforce: 'pre',
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|gltf|glb|babylon|obj|stl)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                    },
+                }, ],
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(appDirectory, "public/index.html"),
+        }),
+    ]
+}
---
+++ webpack.dev.js
@@ -0,0 +1,21 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+const fs = require('fs');
+
+// App directory
+const appDirectory = fs.realpathSync(process.cwd());
+ 
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.resolve(appDirectory, "public"),
+        compress: true,
+        hot: true,
+        publicPath: '/',
+        open: true,
+        // host: '0.0.0.0', // enable to access from other devices on the network
+        // https: true // enable when HTTPS is needed (like in WebXR)
+    },
+});
