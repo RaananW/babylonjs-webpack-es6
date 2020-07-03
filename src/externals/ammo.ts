e24c17f329d64e89d831eb017cdb9197690f3204
Raanan Weber <info@raananweber.com>
2020-07-03 10:28:51 +0200

    ammo

--- src/externals/ammo.ts
+++ src/externals/ammo.ts
@@ -2,7 +2,7 @@ import * as Ammo from "ammo.js";
 
 export let ammoModule: any;
 export const ammoReadyPromise = new Promise((resolve) => {
-    Ammo().then((res: unknown) => {
+    new Ammo().then((res: unknown) => {
         ammoModule = res;
         resolve(res);
     });
