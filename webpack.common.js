81a590708ccd82f567ff9f04b58085c35b28b997
Raanan Weber <info@raananweber.com>
2021-01-13 18:14:24 +0100

    updating dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,7 @@
-const path = require('path');
-const fs = require('fs');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {
-    CleanWebpackPlugin
-} = require('clean-webpack-plugin');
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 // App directory
 const appDirectory = fs.realpathSync(process.cwd());
@@ -11,31 +9,45 @@ const appDirectory = fs.realpathSync(process.cwd());
 module.exports = {
     entry: path.resolve(appDirectory, "src/index.ts"),
     output: {
-        filename: 'js/babylonBundle.js',
+        filename: "js/babylonBundle.js",
+        path: path.resolve("./dist/"),
     },
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: [".ts", ".js"],
+        fallback: {
+            fs: false,
+            path: false, // require.resolve("path-browserify")
+        },
     },
     module: {
-        rules: [{
+        rules: [
+            {
+                test: /\.m?js/,
+                resolve: {
+                    fullySpecified: false,
+                },
+            },
+            {
                 test: /\.(js|mjs|jsx|ts|tsx)$/,
-                loader: 'source-map-loader',
-                enforce: 'pre',
+                loader: "source-map-loader",
+                enforce: "pre",
             },
             {
                 test: /\.tsx?$/,
-                loader: 'ts-loader'
+                loader: "ts-loader",
             },
             {
                 test: /\.(png|jpg|gif|env|glb|stl)$/i,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192,
+                        },
                     },
-                }, ],
-            }
-        ]
+                ],
+            },
+        ],
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -44,8 +56,4 @@ module.exports = {
             template: path.resolve(appDirectory, "public/index.html"),
         }),
     ],
-    // Just for ammo
-    node: {
-        fs: 'empty'
-    }
-}
+};
