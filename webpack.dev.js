a0eeb34e6d602e428b486c3c8aa34f0628d0e13d
Raanan Weber <raweber@microsoft.com>
2021-09-17 15:49:05 +0200

    update

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,10 +10,10 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: path.resolve(appDirectory, "public"),
+        static: path.resolve(appDirectory, "public"),
         compress: true,
         hot: true,
-        publicPath: '/',
+        // publicPath: '/',
         open: true,
         // host: '0.0.0.0', // enable to access from other devices on the network
         // https: true // enable when HTTPS is needed (like in WebXR)
